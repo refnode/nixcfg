8dde7a2bdca62efb25359028a55ba32fe7fb43ff
Sven Wilhelm <refnode@gmail.com>
2024-02-15 23:19:49 +0100

    feat(darwin): add minimal nix-darwin / home-manager configuration
    The configuration only takes my MacBook under control of nix-darwin
    with home-manager system, no deep configuration yet.

---
+++ refnode.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  user,
+  ...
+}: {
+  # Don't change this when you change package input. Leave it alone.
+  home.stateVersion = "23.11";
+
+  # specify my home-manager configs
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    curl
+    less
+    htop
+    tmux
+    jq
+    yq
+  ];
+
+  home.sessionVariables = {
+    PAGER = "less";
+    CLICLOLOR = 1;
+    EDITOR = "nvim";
+  };
+
+  home.username = "${user}";
+  home.homeDirectory = pkgs.lib.mkForce "/Users/${user}";
+
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    defaultKeymap = "viins";
+    dotDir = ".config/zsh";
+  };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+}
