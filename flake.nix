1fea2491995da89053be6c9aa2846075e768e5bf
Sven Wilhelm <refnode@gmail.com>
2024-02-16 01:32:48 +0100

    refactor(darwin): move darwinConfiguration into own module
    Next small step: Move the main part of the darwinConfiguration into an
    own module. Finally only a parameterized named configuration should be
    present in the flake file itself.

--- flake.nix
+++ flake.nix
@@ -64,25 +64,11 @@
 
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#defiant
+
     darwinConfigurations = let
       system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
     in {
-      defiant = nix-darwin.lib.darwinSystem {
-        modules = [
-          ./hosts/darwin-base.nix
-          {_module.args = {inherit flake;};}
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${user} = import ./${user}.nix {inherit pkgs user;};
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-            # home-manager.extraSpecialArgs = { inherit lib; };
-          }
-        ];
-      };
+      defiant = import ./lib {inherit nixpkgs nix-darwin home-manager system user flake;};
     };
 
     nixosConfigurations = {};
--- lib/default.nix
+++ lib/default.nix
@@ -1,2 +1,27 @@
-{...}: {
-}
+{
+  nixpkgs,
+  nix-darwin,
+  home-manager,
+  system,
+  user,
+  flake,
+  ...
+}: let
+  # system = "aarch64-darwin";
+  pkgs = nixpkgs.legacyPackages.${system};
+in
+  nix-darwin.lib.darwinSystem {
+    modules = [
+      ../hosts/darwin-base.nix
+      {_module.args = {inherit flake;};}
+      home-manager.darwinModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${user} = import ../${user}.nix {inherit pkgs user;};
+        # Optionally, use home-manager.extraSpecialArgs to pass
+        # arguments to home.nix
+        # home-manager.extraSpecialArgs = { inherit lib; };
+      }
+    ];
+  }
