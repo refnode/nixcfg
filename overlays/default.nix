3340ea75e016f4900629b635ee95f9796c8fe36e
Sven Wilhelm <refnode@gmail.com>
2024-10-13 19:34:30 +0200

    style: align naming of packages dir to commonly used pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,12 +1,1 @@
-final: prev: {
-  ref-rebuild = final.callPackage ../packages/ref-rebuild {};
-  ref-sysupdate = final.callPackage ../packages/ref-sysupdate {};
-  ref-ykey-check = final.callPackage ../packages/ref-ykey-check {};
-  ref-main = final.callPackage ../packages/ref-main {};
-  ref-mail = final.callPackage ../packages/ref-mail {};
-  ref-mail-sync = final.callPackage ../packages/ref-mail-sync {};
-  ref-mail-cd = final.callPackage ../packages/ref-mail-cd {};
-  ref-mail-cdv = final.callPackage ../packages/ref-mail-cdv {};
-  ref-irc = final.callPackage ../packages/ref-irc {};
-  ref-pathlist = final.callPackage ../packages/ref-pathlist {};
-}
+final: prev: import ../pkgs {pkgs = final;}
