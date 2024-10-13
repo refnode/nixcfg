41905320bee6326e72fd6a69e1ad3d620acde4e1
Sven Wilhelm <refnode@gmail.com>
2024-10-13 19:34:30 +0200

    style: align naming of packages dir to commonly used pkgs

---
+++ pkgs/default.nix
@@ -0,0 +1,12 @@
+{pkgs ? import <nixpkgs> {}, ...}: rec {
+  ref-rebuild = pkgs.callPackage ../pkgs/ref-rebuild {};
+  ref-sysupdate = pkgs.callPackage ../pkgs/ref-sysupdate {};
+  ref-ykey-check = pkgs.callPackage ../pkgs/ref-ykey-check {};
+  ref-main = pkgs.callPackage ../pkgs/ref-main {};
+  ref-mail = pkgs.callPackage ../pkgs/ref-mail {};
+  ref-mail-sync = pkgs.callPackage ../pkgs/ref-mail-sync {};
+  ref-mail-cd = pkgs.callPackage ../pkgs/ref-mail-cd {};
+  ref-mail-cdv = pkgs.callPackage ../pkgs/ref-mail-cdv {};
+  ref-irc = pkgs.callPackage ../pkgs/ref-irc {};
+  ref-pathlist = pkgs.callPackage ../pkgs/ref-pathlist {};
+}
