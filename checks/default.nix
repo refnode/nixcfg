d3da1e0d5b38cee65b20f21e6be700bb7d62d50a
Sven Wilhelm <refnode@gmail.com>
2024-11-08 22:01:55 +0100

    build: active common checks for typical format weaknesses

--- checks/default.nix
+++ checks/default.nix
@@ -6,10 +6,27 @@
   pre-commit-check = pre-commit-hooks.lib.${system}.run {
     src = ./.;
     hooks = {
+      # common checks checks
+      trim-trailing-whitespace.enable = true;
+      mixed-line-endings.enable = true;
+      end-of-file-fixer.enable = true;
+      check-added-large-files.enable = true;
+      check-case-conflicts.enable = true;
+      check-merge-conflicts.enable = true;
+
+      # nix file checks
       alejandra.enable = true;
-      shellcheck.enable = true;
-      yamllint.enable = true;
+
+      # markdown checks
       markdownlint.enable = true;
+
+      # yaml checks
+      yamllint.enable = true;
+
+      # shell script checks
+      shellcheck.enable = true;
+
+      # GitHub actions checks
       actionlint.enable = true;
     };
   };
