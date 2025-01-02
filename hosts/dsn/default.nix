19b91f5c5576e9431676c467c31595df1632b4f7
Sven Wilhelm <refnode@gmail.com>
2025-01-02 12:19:41 -0500

    fixup airgapped

--- hosts/dsn/default.nix
+++ hosts/dsn/default.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   modulesPath,
   inputs,
   outputs,
@@ -17,6 +18,15 @@
   };
 
   system.stateVersion = "24.11";
+
+  # Disable networking on boot and for the running system
+  # See https://search.nixos.org/options
+  boot.initrd.network.enable = lib.mkForce false;
+  networking = {
+    dhcpcd.enable = lib.mkForce false;
+    wireless.enable = lib.mkForce false;
+    networkmanager.enable = lib.mkForce false;
+  };
 
   environment.systemPackages = with pkgs; [
     neovim
