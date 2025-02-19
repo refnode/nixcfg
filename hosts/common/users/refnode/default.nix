5330e958bfbaebe02f993c1fb79ab7b94a8496cb
Sven Wilhelm <refnode@gmail.com>
2025-02-20 00:55:41 +0100

    feat(refnode/darwin): enable service aerospace as window manager
    Enable the service aerospace wm and take the configuration to
    nix control.

--- hosts/common/users/refnode/default.nix
+++ hosts/common/users/refnode/default.nix
@@ -1,3 +1,11 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home-manager.users.refnode = import ../../../../users/refnode/default.nix;
+
+  imports = [
+    ../../../../users/refnode/common/darwin
+  ];
 }
