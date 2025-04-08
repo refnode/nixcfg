0214577824929b9992cb724f55832048ad5cd7ba
Sven Wilhelm <refnode@gmail.com>
2025-04-08 19:43:12 +0200

    chore: define home-manager backup file extension

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -28,6 +28,10 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  # define backup file extension to enable automatic moves
+  # of existing files as backup during initial takeover of config files
+  # instead of failing hm runs.
+  home-manager.backupFileExtension = "hm-backup";
   home-manager.extraSpecialArgs = {
     inherit pkgs inputs;
   };
