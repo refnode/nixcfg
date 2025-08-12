d7190f3e34fe9ed2d21d89dd571a3d4c38a432db
Sven Wilhelm <refnode@gmail.com>
2025-08-12 15:08:58 +0200

    fix: complete development tools modularization
    Move remaining development tools to development module:
    
    - Add garden, mob, universal-ctags, jujutsu

--- users/refnode/common/optional/development.nix
+++ users/refnode/common/optional/development.nix
@@ -10,11 +10,17 @@
     gh
     unstable.lazygit
     unstable.glab
+    garden
     
     # refnode's own tooling
     ref-git-bare-clone-update
     
     # Development utilities
     cookiecutter
+    universal-ctags
+    unstable.mob
+    
+    # Jujutsu VCS
+    unstable.jujutsu
   ];
 }
